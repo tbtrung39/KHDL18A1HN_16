06b523bd7d2d3ed4aa51bdb0508a7b8e8d9c7f41
HuaDuy245 <hhduy.24174600017@sv.uneti.edu.vn>
2025-02-19 21:53:08 +0700

    bai_05.py

---
+++ Bài tập thực hành/bai_05.py
@@ -0,0 +1,4 @@
+a1, a2, a3 = map(float, input("Nhập tọa độ vector a (a1 a2 a3): ").split())
+b1, b2, b3 = map(float, input("Nhập tọa độ vector b (b1 b2 b3): ").split())
+tich_vo_huong = a1 * b1 + a2 * b2 + a3 * b3
+print('Tích vô hướng của hai vector a và b là:',tich_vo_huong) 
