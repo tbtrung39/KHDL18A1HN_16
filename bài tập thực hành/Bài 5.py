ca74ef8485c21ac55e807105865a035bab52f948
dieulinh0604 <ntdlinh.24174600054@sv.uneti.edu.vn>
2025-02-19 21:58:57 +0700

    Update Bài 5.py

---
+++ bài tập thực hành/Bài 5.py
@@ -0,0 +1,9 @@
+n = int(input("Nhập kích thước của các vector: "))
+a = []
+for i in range(n):
+    a.append(float(input(f"Nhập phần tử thứ {i + 1} của vector a: ")))
+b = []
+for i in range(n):
+    b.append(float(input(f"Nhập phần tử thứ {i + 1} của vector b: ")))
+tich_vo_huong = sum(a[i] * b[i] for i in range(n))
+print(f"Tích vô hướng của hai vector là: {tich_vo_huong}")
