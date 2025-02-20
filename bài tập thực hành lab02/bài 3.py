74f45966480c1d4ef97f771e25c095f9cfd3297c
dieulinh0604 <ntdlinh.24174600054@sv.uneti.edu.vn>
2025-02-20 21:50:00 +0700

    Update bài 3.py

---
+++ bài tập thực hành lab02/bài 3.py
@@ -0,0 +1,17 @@
+thu_trong_tuan = int(input("Nhập vào các thứ (1->7) trong tuần: "))
+if  thu_trong_tuan == 1:
+    print("1:Monday")
+elif thu_trong_tuan  == 2:
+    print("2:Tuesday")
+elif  thu_trong_tuan == 3:
+    print("3:Wednesday")
+elif thu_trong_tuan  == 4:
+    print("4:Thursday")
+elif thu_trong_tuan  == 5:
+    print("5:Friday")
+elif thu_trong_tuan  == 6:
+    print("6:Saturday")
+elif thu_trong_tuan  == 7:
+    print("Sunday")
+else :
+    print("Nhập sai vui lòng nhập lại!")
