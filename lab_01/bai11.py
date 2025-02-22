978902868f74ee4547518a305cf0d0f04085944f
tddatuneti <tddat.24174600032@sv.uneti.edu.vn>
2025-02-22 15:23:24 +0700

    Update bai11.py

---
+++ lab_01/bai11.py
@@ -0,0 +1,5 @@
+# Bài 11: Xác suất lăn 2 xúc xắc có ít nhất 1 lần ra 3
+so_cach_ra_3 = 11
+tong_so_cach = 36
+xac_suat = so_cach_ra_3 / tong_so_cach
+print("Xác suất lăn 2 xúc xắc có ít nhất 1 lần ra 3:", round(xac_suat, 4))
