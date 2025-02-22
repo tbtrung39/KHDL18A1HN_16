db3ad3362572e548ee8d3d5f7acc2c64cbded644
tddatuneti <tddat.24174600032@sv.uneti.edu.vn>
2025-02-22 15:21:33 +0700

    Update bai5.py

---
+++ lab_01/bai5.py
@@ -0,0 +1,7 @@
+# Bài 5: Tính tích vô hướng của hai vector
+a_x = float(input("Nhập tọa độ x của vector a: "))
+a_y = float(input("Nhập tọa độ y của vector a: "))
+b_x = float(input("Nhập tọa độ x của vector b: "))
+b_y = float(input("Nhập tọa độ y của vector b: "))
+tich_vo_huong = a_x * b_x + a_y * b_y
+print("Tích vô hướng của hai vector:", round(tich_vo_huong, 2))
