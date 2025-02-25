b7538eb00ff4c84ae9580bd1f809758ca656d1fe
nttlinh.24174600012@sv.uneti.edu.vn <nttlinh.24174600012@example.com>
2025-02-25 19:09:47 +0700

    update lab01 vì lỗi lưu bài

---
+++ Thuc_hanh_tren_lop/bai5.py
@@ -0,0 +1,6 @@
+a_x = float(input("Nhập tọa độ x của vector a: "))
+a_y = float(input("Nhập tọa độ y của vector a: "))
+b_x = float(input("Nhập tọa độ x của vector b: "))
+b_y = float(input("Nhập tọa độ y của vector b: "))
+tich_vo_huong = a_x * b_x + a_y * b_y
+print("Tích vô hướng của hai vector:", round(tich_vo_huong, 2))
---
+++ Thuc_hanh_tren_lop/bai9.py
@@ -0,0 +1,9 @@
+x1 = float(input("Nhập tọa độ x của điểm A: "))
+y1 = float(input("Nhập tọa độ y của điểm A: "))
+x2 = float(input("Nhập tọa độ x của điểm B: "))
+y2 = float(input("Nhập tọa độ y của điểm B: "))
+x3 = float(input("Nhập tọa độ x của điểm C: "))
+y3 = float(input("Nhập tọa độ y của điểm C: "))
+x_trong_tam = (x1 + x2 + x3) / 3
+y_trong_tam = (y1 + y2 + y3) / 3
+print("Tọa độ trọng tâm tam giác:", (round(x_trong_tam, 2), round(y_trong_tam, 2)))
