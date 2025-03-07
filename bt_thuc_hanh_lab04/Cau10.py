5457c33fcb2ce84872b8d0f309f0ea9cb7091a78
TUAN HUNG <tthung.24174600051@sv.uneti.edu.vn>
2025-03-07 20:19:08 +0700

    Đã hoàn thành bài tập phòng máy lab04

---
+++ bt_thuc_hanh_lab04/Cau10.py
@@ -0,0 +1,14 @@
+# Câu 10
+chuso = ["không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"]
+n = int(input("Nhập số: "))
+if n == 0:
+    print("không")
+else:
+    ketqua = "" 
+
+    while n > 0:
+        chusocuoi = n % 10  
+        ket_ua = chuso[chusocuoi] + " " + ketqua  
+        n = n // 10  
+
+    print("Kết quả:", ketqua.strip())  
---
+++ bt_thuc_hanh_lab04/Cau8.py
@@ -0,0 +1,7 @@
+# Câu 8
+while True:
+    chu = input("Nhập một ký tự: ")
+    if len(chu) == 1:  # Kiểm tra nhập đúng 1 ký tự
+        break
+    print("Vui lòng nhập một ký tự duy nhất!")
+print(f"Giá trị ASCII của '{chu}' là {ord(chu)}")
