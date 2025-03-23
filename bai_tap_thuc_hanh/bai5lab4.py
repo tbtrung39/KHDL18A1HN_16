8351d03f8bde7d95b944e4bbae451bf9fa6dc0c2
unknown <vdanh.24174600050@sv.uneti.edu.vn>
2025-03-23 22:45:56 +0700

    em da lam xong lab5

--- bai_tap_thuc_hanh/bai5lab4.py
+++
@@ -1,5 +0,0 @@
-while True:
-    num = int(input("Nhập số: "))
-    if num < 0:
-        print("Đã nhập số âm, dừng chương trình.")
-        break
--- bai_tap_thuc_hanh/bai9lab4.py
+++
@@ -1,9 +0,0 @@
-num = input("Nhập một số nguyên: ")
-i = 0
-tong = 0
-
-while i < len(num):
-    tong += int(num[i])
-    i += 1
-
-print("Tổng các chữ số là:", tong)
