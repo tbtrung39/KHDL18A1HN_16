d9b1c568c387a915c565e6e9c384a8072c26d314
unknown <vdanh.24174600050@sv.uneti.edu.vn>
2025-03-31 10:18:56 +0700

    em da lam xong lab6

---
+++ baitapthuchanh_lab6/bai17.py
@@ -0,0 +1,4 @@
+n = int(input("Nhap n: "))
+A = [[1 if i == j else 0 for j in range(n)] for i in range(n)]
+for row in A:
+    print(row)
---
+++ baitapthuchanh_lab6/bai5.py
@@ -0,0 +1,6 @@
+import random
+A = []
+for _ in range(1000):
+    num = random.randint(1, 999999)  
+    A.append(num)  
+print(A)
