f0fd8b9b02d601c3fe4e593e54ac1fb4432619a1
unknown <lhcham.24174600059@sv.uneti.edu.vn>
2025-03-31 23:22:45 +0700

    ộp bài tập lab 06

---
+++ LAB02/Bai_03.py
@@ -0,0 +1,17 @@
+thu=int(input("Nhap vao mot so tu 1 den 7:"))
+if thu==1:
+    print("Sunday")
+elif thu==2:
+    print("Monday")
+elif thu==3:
+    print("Tuesday")
+elif thu==4:
+    print("Wenesday")
+elif thu==5:
+    print("Thursday")
+elif thu==6:
+    print("Saturday")
+elif thu==7:
+    print("Friday")
+else:
+    print("So khong hop le. Vui long nhap lai.")
