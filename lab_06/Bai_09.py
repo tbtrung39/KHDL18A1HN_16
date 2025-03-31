76ac40637420b1f963952934b46392377946867d
Nguyen Vu An <nvan.2417460025@sv.uneti.edu.vn>
2025-03-31 22:04:46 +0700

    lab 06

---
+++ lab_06/Bai_09.py
@@ -0,0 +1,3 @@
+numbers = list(map(int, input("Nhap cac so nguyen, cach nhau boi dau cach: ").split()))  
+assert all(num % 2 == 0 for num in numbers), "Danh sach chua so le!"  
+print("Danh sach hop le, tat ca cac so deu la so chan:", numbers)
