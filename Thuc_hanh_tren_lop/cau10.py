f9751d75db66e54d3eefa7789cd690cb3f78d016
nttlinh.24174600012@sv.uneti.edu.vn <nttlinh.24174600012@example.com>
2025-04-27 22:49:18 +0700

    đã hoàn thành lab9

---
+++ Thuc_hanh_tren_lop/cau10.py
@@ -0,0 +1,11 @@
+def tinh_X(n, X_cache=None):
+    if X_cache is None:
+        X_cache = {0: 1}  
+    if n in X_cache:
+        return X_cache[n]
+    X_n = sum((i ** 2) * tinh_X(i, X_cache) for i in range(n))
+    X_cache[n] = X_n
+    return X_n
+n = int(input("Nhập giá trị n: "))
+Xn = tinh_X(n)
+print(f"Giá trị của X_{n} là: {Xn}")
---
+++ Thuc_hanh_tren_lop/cau6.py
@@ -0,0 +1,9 @@
+import random
+n = int(input("Nhập số tự nhiên n: "))
+A = [i for i in range(1, n + 1)]
+result = []
+while A:
+    x = random.choice(A)  
+    result.append(x)      
+    A.remove(x)           
+print("Hoán vị ngẫu nhiên:", result)
