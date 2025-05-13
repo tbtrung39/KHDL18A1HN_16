2c49c50168a3c4d8defc18dd89b1a4d4641da933
thuylinh2212 <lttlinh.24174600015@sv.uneti.edu.vn>
2025-05-13 22:59:50 +0700

    hoan thanh lab 12

---
+++ bai tap thuc hanh/LAB12/bai9.py
@@ -0,0 +1,14 @@
+import datetime
+
+try:
+    ngay = int(input("Nhập ngày: "))
+    thang = int(input("Nhập tháng: "))
+    nam = int(input("Nhập năm: "))
+
+    d = datetime.datetime(nam, thang, ngay)
+    tuan = d.isocalendar()[1]
+
+    print(f"Ngày {d.strftime('%d-%m-%Y')} thuộc tuần thứ {tuan} của năm.")
+
+except ValueError:
+    print("Lỗi: Ngày không hợp lệ!")
