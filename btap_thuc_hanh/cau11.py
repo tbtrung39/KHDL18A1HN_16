b6028e0164337d45c596da9eabff9efcca09af6b
Huu Dang <nguyenhhuhaidang6@gmail.com>
2025-05-14 13:45:44 +0700

    da hoan thanh lap 10

---
+++ btap_thuc_hanh/cau11.py
@@ -0,0 +1,13 @@
+import sys
+sys.path.append("C://KHDL18A1HN_16//bai_tap_thuc_hanh//cau_11.py")
+# main_doicoso.py
+
+from cau_5 import dec_to_bin,dec_to_hex,dec_to_oct
+from cau_6 import hex_to_dec,oct_to_dec,binary_to_dec 
+
+print(dec_to_bin(25))
+print(binary_to_dec("11001"))
+print(dec_to_hex(7))
+print(dec_to_oct(4))
+print(hex_to_dec("3"))
+print(oct_to_dec("5"))
---
+++ btap_thuc_hanh/cau6.py
@@ -0,0 +1,12 @@
+
+def is_binary_string(s):
+    return all(c in '01' for c in s)
+
+def binary_to_dec(s):
+    return int(s, 2)
+
+def oct_to_dec(s):
+    return int(s,10)
+
+def hex_to_dec(s):
+    return int(s, 16)
