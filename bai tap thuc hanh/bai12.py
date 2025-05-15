7d2f31f4ea655a0a577fd5ec4ecf4927b67cd9f4
luong thi cuc <ltcuc.24174600178@sv.uneti.edu.vn>
2025-05-15 19:59:30 +0700

    đã hoàn thành lab08

---
+++ bai tap thuc hanh/bai12.py
@@ -0,0 +1,16 @@
+def thong_tin_nv(ds):
+    ds["Họ tên"] = input("Nhập họ tên: ")
+    ds["Quê quán"] = input("Nhập quê quán: ")
+    ds["Thâm niên"] = float(input("Nhập thâm niên: "))
+    ds["Lương"] = tinh_lương(ds)
+    return ds
+def tinh_lương(ds):
+    luong = float(input("Nhập lương: "))
+    for _ in ds:
+        luong = luong + (1 + 0.05 * ds["Thâm niên"])
+    return luong
+def xuat_thong_tin(ds):
+    print(thong_tin_nv(ds))
+ds = {}
+xuat_thong_tin(ds)
+    
