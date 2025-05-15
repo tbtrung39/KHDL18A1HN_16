162b84387b70818da0e31ca28b38198a7b10a0a8
MinhDao26 <dqminh.24174600043@sv.uneti.edu.vn>
2025-05-15 21:19:49 +0700

    hoan thanh lab12

---
+++ bai3.py
@@ -0,0 +1,15 @@
+try:
+    file_name = input("Nhập tên tệp cần đọc: ")
+
+    with open(file_name, "r", encoding="utf-8") as f_in:
+        content = f_in.read()
+
+    with open("a.dat", "w", encoding="utf-8") as f_out:
+        f_out.write(content)
+
+    print("Đã sao chép nội dung sang copy.dat.")
+
+except FileNotFoundError:
+    print("Lỗi: Không tìm thấy tệp.")
+except Exception as e:
+    print("Đã xảy ra lỗi:", e)
